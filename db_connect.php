b6de652035858a7df767df0e7d2d00f38f935a8c
jirehtulung <163143773+jirehtulung@users.noreply.github.com>
2024-05-21 23:50:39 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = ""; 
 $dbname = "portofolio"; 
 
-// Membuat koneksi
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Mengecek koneksi
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
